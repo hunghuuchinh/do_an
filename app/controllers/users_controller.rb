b3adf9e5f5781311b3be0c07d14f33ffec49043e
hunghuuchinh <hungbka2010@gmail.com>
2015-11-27 05:59:59 +0700

    8.31

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
    def create
     @user = User.new(user_params)
     if @user.save
+    	log_in @user
     	flash[:success] = "Welcome to the San Bong Ha Noi!"
-    	redirect_to root_path
+    	 redirect_to @user
       # Handle a successful save.
     else
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ActiveRecord::Base
   validates :phone, presence: true, length: { minimum: 9, maximum: 12}
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+    # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+   def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
